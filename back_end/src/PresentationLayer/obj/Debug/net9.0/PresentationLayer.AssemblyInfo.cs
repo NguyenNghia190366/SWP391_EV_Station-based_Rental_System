7ba11f1988f33391acc9f00bdc147e602bb39a20
Gia Huy <onggiahuy222@gmail.com>
2025-10-22 01:16:17 +0700

    modified: obj.

--- back_end/src/PresentationLayer/obj/Debug/net9.0/PresentationLayer.AssemblyInfo.cs
+++ back_end/src/PresentationLayer/obj/Debug/net9.0/PresentationLayer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PresentationLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94a5fa119f14c5c3ec7cc843d6cc8672faa6961f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bca47413c546bda86fa3ad1a5627eecd3c23f262")]
 [assembly: System.Reflection.AssemblyProductAttribute("PresentationLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PresentationLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
