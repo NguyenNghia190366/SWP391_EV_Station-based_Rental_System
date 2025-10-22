c3bd04ce5c2d7a6aa8082b0a5e0d0172471b30f0
Gia Huy <onggiahuy222@gmail.com>
2025-10-22 16:21:26 +0700

    add: Render port.

--- back_end/src/BusinessLogicLayer/obj/Debug/net9.0/BusinessLogicLayer.AssemblyInfo.cs
+++ back_end/src/BusinessLogicLayer/obj/Debug/net9.0/BusinessLogicLayer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BusinessLogicLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bca47413c546bda86fa3ad1a5627eecd3c23f262")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79c0c574cb2c4c74a57957034818a5895827f76c")]
 [assembly: System.Reflection.AssemblyProductAttribute("BusinessLogicLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BusinessLogicLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
