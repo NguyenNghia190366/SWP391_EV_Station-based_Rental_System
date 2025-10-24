22e6d58964123014baa2936f814397767500fcee
Nguyen Trung Nghia <nghia011104@gmail.com>
2025-10-24 13:07:23 +0700

    7th

---
+++ DEMO01_EV_rental_System/Controllers/UserAccountController.cs
@@ -0,0 +1,31 @@
+﻿using DEMO01_EV_rental_System.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DEMO01_EV_rental_System.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserAccountController : ControllerBase
+    {
+        private readonly JwtService _jwtService;
+        public UserAccountController(JwtService jwtService)
+        {
+            _jwtService = jwtService;
+        }
+
+        [AllowAnonymous]
+        [HttpPost("login")]
+        public async Task<ActionResult<LoginResponseDTO>> Login(LoginRequestDTO request)
+        {
+            var result = await _jwtService.Authenticate(request);
+            if(result is null)
+            {
+                return Unauthorized();
+            }
+            return result;
+        }
+
+    }
+}
---
+++ DEMO01_EV_rental_System/Entities/Station.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DEMO01_EV_rental_System.Entities;
+
+public partial class Station
+{
+    public int StationId { get; set; }
+
+    public string StationName { get; set; } = null!;
+
+    public string Address { get; set; } = null!;
+
+    public decimal? Latitude { get; set; }
+
+    public decimal? Longitude { get; set; }
+
+    public string Status { get; set; } = null!;
+
+    public int Capacity { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+
+    public virtual ICollection<RentalOrder> RentalOrderPickupStations { get; set; } = new List<RentalOrder>();
+
+    public virtual ICollection<RentalOrder> RentalOrderReturnStations { get; set; } = new List<RentalOrder>();
+
+    public virtual ICollection<Staff> Staff { get; set; } = new List<Staff>();
+
+    public virtual ICollection<Vehicle> Vehicles { get; set; } = new List<Vehicle>();
+}
