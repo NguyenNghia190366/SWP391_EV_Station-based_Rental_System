310afee708433a361531b57c1e2b7ec3702aa883
Gia Huy <onggiahuy222@gmail.com>
2025-10-25 22:13:37 +0700

    modified: obj.

--- back_end/src/DataAccessLayer/obj/Debug/net9.0/DataAccessLayer.AssemblyInfo.cs
+++ back_end/src/DataAccessLayer/obj/Debug/net9.0/DataAccessLayer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataAccessLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12c38c9d9999d640823982168a10fee88c64f969")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31d3ed485ebf62dedb93d39dc70b1783f5e956fb")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataAccessLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataAccessLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
