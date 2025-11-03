6db05f700c31d60757cfe229bd1b8b00d134bbd4
Gia Huy <onggiahuy222@gmail.com>
2025-11-04 02:41:22 +0700

    add: get StaffID, RenterId login.

--- back_end/src/BusinessLogicLayer/DTOs/Auth/LoginResponse.cs
+++ back_end/src/BusinessLogicLayer/DTOs/Auth/LoginResponse.cs
@@ -3,7 +3,10 @@ namespace BusinessLogicLayer.DTOs.Auth
     public class LoginResponse
     {
         public string Token { get; set; } = string.Empty;
-        public string Role { get; set; } = string.Empty;   // "Admin" | "Staff" | "Renter"
+        public string Role { get; set; } = string.Empty;   // "ADMIN" | "STAFF" | "RENTER"
         public int UserId { get; set; }
+
+        public int? StaffId { get; set; }   // null nếu Role != "STAFF"
+        public int? RenterId { get; set; }  // null nếu Role != "RENTER"
     }
 }
