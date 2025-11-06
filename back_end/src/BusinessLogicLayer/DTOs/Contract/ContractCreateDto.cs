41b0b6b5c3b074eedbb7b75a705f7d97debed327
Gia Huy <onggiahuy222@gmail.com>
2025-11-06 18:49:30 +0700

    modified: Contract DTO: add staffId.

--- back_end/src/BusinessLogicLayer/DTOs/Contract/ContractCreateDto.cs
+++ back_end/src/BusinessLogicLayer/DTOs/Contract/ContractCreateDto.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,15 +7,7 @@ namespace BusinessLogicLayer.DTOs.Contract
 {
     public class ContractCreateDto
     {
-        [Required]
         public int OrderId { get; set; } 
-        
-        // Thêm trường này theo nghiệp vụ "Nhận xe" 
-        // Bảng RentalOrder cũng có cột "img_vehicle_before_URL" 
-        [Required]
-        [Url] // Đảm bảo đây là một URL hợp lệ
-        public string ImgVehicleBeforeUrl { get; set; } = string.Empty;
-
-        // public int StaffId { get; set; } // Tốt! Ta vẫn lấy từ Claims
+        // public int StaffId { get; set; } // lấy từ User.Claims (context người dùng hiện tại)
     }
 }
