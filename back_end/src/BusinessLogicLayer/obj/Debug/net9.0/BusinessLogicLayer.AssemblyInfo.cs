bfa2efbdf2d970c9e68e267c2ec07563b6e02621
Gia Huy <onggiahuy222@gmail.com>
2025-11-08 01:41:23 +0700

    modified: obj.

--- back_end/src/BusinessLogicLayer/obj/Debug/net9.0/BusinessLogicLayer.AssemblyInfo.cs
+++ back_end/src/BusinessLogicLayer/obj/Debug/net9.0/BusinessLogicLayer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BusinessLogicLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3fbf1c944d3dbf8e7521ec749a7de3bb0ea5e6b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a55ac1f7fafc7629338105f2bf9be6c7389c79c")]
 [assembly: System.Reflection.AssemblyProductAttribute("BusinessLogicLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BusinessLogicLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
