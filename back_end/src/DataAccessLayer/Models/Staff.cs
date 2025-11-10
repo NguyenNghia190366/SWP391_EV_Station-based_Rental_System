0b7452ae4a5371ff1e19a01ab5bd03fa40ff1c96
Gia Huy <onggiahuy222@gmail.com>
2025-11-10 23:06:27 +0700

    modified: add Constructor, RentalOrderpickup_staffs, RentalOrderreturn_staffs.

--- back_end/src/DataAccessLayer/Models/Staff.cs
+++ back_end/src/DataAccessLayer/Models/Staff.cs
@@ -9,6 +9,15 @@ namespace DataAccessLayer.Models;
 [Index("user_id", Name = "UQ__Staff__B9BE370EF13222F0", IsUnique = true)]
 public partial class Staff
 {
+    public Staff()
+    {
+        Contracts = new HashSet<Contract>();
+
+        // THÊM 2 DÒNG MỚI NÀY VÀO CONSTRUCTOR:
+        RentalOrderpickup_staffs = new HashSet<RentalOrder>();
+        RentalOrderreturn_staffs = new HashSet<RentalOrder>();
+    }
+    
     [Key]
     public int staff_id { get; set; }
 
@@ -18,6 +27,10 @@ public partial class Staff
 
     [InverseProperty("staff")]
     public virtual ICollection<Contract> Contracts { get; set; } = new List<Contract>();
+
+    // THÊM 2 DÒNG MỚI NÀY (ở cuối file):
+    public virtual ICollection<RentalOrder> RentalOrderpickup_staffs { get; set; }
+    public virtual ICollection<RentalOrder> RentalOrderreturn_staffs { get; set; }
 
     [ForeignKey("station_id")]
     [InverseProperty("Staff")]
