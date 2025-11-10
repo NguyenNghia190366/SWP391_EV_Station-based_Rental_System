8fd7e80c57bd27274b60bfc04a7b54cae928b197
Gia Huy <onggiahuy222@gmail.com>
2025-11-11 01:51:59 +0700

    modified: Renter DTOs.

--- back_end/src/BusinessLogicLayer/DTOs/Renter/RenterDocumentsUpsertDto.cs
+++ back_end/src/BusinessLogicLayer/DTOs/Renter/RenterDocumentsUpsertDto.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,9 +8,29 @@ namespace BusinessLogicLayer.DTOs.Renter
 {
     public class RenterDocumentsUpsertDto
     {
+        // --- CCCD ---
+        [Required]
+        [StringLength(12, MinimumLength = 9)] 
         public string IdCardNumber { get; set; } = string.Empty;
-        public string IdCardImageUrl { get; set; } = string.Empty;
+        [Required]
+        [Url]
+        public string UrlCccdCmndFront { get; set; } = string.Empty;
+
+        [Required]
+        [Url]
+        public string UrlCccdCmndBack { get; set; } = string.Empty;
+
+        // --- GPLX ---
+        [Required]
+        [StringLength(12, MinimumLength = 12)] 
         public string DriverLicenseNumber { get; set; } = string.Empty;
-        public string DriverLicenseImageUrl { get; set; } = string.Empty;
+
+        [Required]
+        [Url]
+        public string UrlDriverLicenseFront { get; set; } = string.Empty;
+
+        [Required]
+        [Url]
+        public string UrlDriverLicenseBack { get; set; } = string.Empty;
     }
 }
--- back_end/src/BusinessLogicLayer/DTOs/Renter/RenterDocumentsViewDto.cs
+++ back_end/src/BusinessLogicLayer/DTOs/Renter/RenterDocumentsViewDto.cs
@@ -7,10 +7,18 @@ namespace BusinessLogicLayer.DTOs.Renter
 {
     public class RenterDocumentsViewDto
     {
-        public string IdCardNumber { get; set; } = string.Empty;       // CCCD/CMND
-        public string IdCardImageUrl { get; set; } = string.Empty;
-        public string DriverLicenseNumber { get; set; } = string.Empty;    // GPLX
-        public string DriverLicenseImageUrl { get; set; } = string.Empty;
-        public bool IsVerified { get; set; }  // Trạng thái xác thực của Renter
+        public string? IdCardNumber { get; set; }
+        public string? DriverLicenseNumber { get; set; }
+        
+        // --- SỬA/THÊM 4 TRƯỜNG MỚI ---
+        
+        // URL ẢNH CCCD
+        public string? UrlCccdCmndFront { get; set; }
+        public string? UrlCccdCmndBack { get; set; }
+        // URL ẢNH GPLX
+        public string? UrlDriverLicenseFront { get; set; }
+        public string? UrlDriverLicenseBack { get; set; }
+
+        public bool IsVerified { get; set; }
     }
 }
