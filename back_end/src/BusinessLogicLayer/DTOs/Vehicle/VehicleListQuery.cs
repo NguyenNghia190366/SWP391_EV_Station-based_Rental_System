fe3dee0454c35438612cf1c8c617c1b87feb962e
Gia Huy <onggiahuy222@gmail.com>
2025-11-10 23:32:03 +0700

    modified: property: MinBattery (int?) -> (decimal?).

--- back_end/src/BusinessLogicLayer/DTOs/Vehicle/VehicleListQuery.cs
+++ back_end/src/BusinessLogicLayer/DTOs/Vehicle/VehicleListQuery.cs
@@ -8,9 +8,7 @@ namespace BusinessLogicLayer.DTOs.Vehicles
         public int? StationId { get; set; } // Filter theo trạm
         public int? VehicleModelId { get; set; } // Filter theo model
         public bool? IsAvailable { get; set; } // Filter xe có sẵn
-        
-        [Range(0, 100)]
-        public int? MinBattery { get; set; } // Filter pin tối thiểu
+        public decimal? MinBattery { get; set; } // Filter pin tối thiểu
 
         public int Page { get; set; } = 1;
         public int PageSize { get; set; } = 10;
