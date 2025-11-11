fee396a2965e6b88b77082692a848212e3fac6d8
Gia Huy <onggiahuy222@gmail.com>
2025-11-12 01:42:18 +0700

    add: property ReportListQuery.

--- back_end/src/BusinessLogicLayer/DTOs/Report/ReportListQuery.cs
+++ back_end/src/BusinessLogicLayer/DTOs/Report/ReportListQuery.cs
@@ -1,12 +1,12 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
+// BusinessLogicLayer/DTOs/Report/ReportListQuery.cs
 namespace BusinessLogicLayer.DTOs.Report
 {
     public class ReportListQuery
     {
-        
+        public int? StaffId { get; set; }
+        public int? OrderId { get; set; }
+
+        public int Page { get; set; } = 1;
+        public int PageSize { get; set; } = 10;
     }
 }
