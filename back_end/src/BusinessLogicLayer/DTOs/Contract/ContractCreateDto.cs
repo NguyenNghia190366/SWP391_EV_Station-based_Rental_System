c2e76f170d7c9c87cc2a82fbe04b92da63a75c83
Gia Huy <onggiahuy222@gmail.com>
2025-11-16 00:28:32 +0700

    add: Contract DTOs.

--- back_end/src/BusinessLogicLayer/DTOs/Contract/ContractCreateDto.cs
+++ back_end/src/BusinessLogicLayer/DTOs/Contract/ContractCreateDto.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,5 +10,15 @@ namespace BusinessLogicLayer.DTOs.Contract
     {
         public int OrderId { get; set; } 
         // public int StaffId { get; set; } // lấy từ User.Claims (context người dùng hiện tại)
+        /// <summary>
+        /// Danh sách các URL ảnh xe trước khi bàn giao
+        /// </summary>
+        public List<string>? ImgVehicleBeforeUrls { get; set; }
+
+        /// <summary>
+        /// Số CCCD của nhân viên giao xe (nếu cần)
+        /// </summary>
+        [StringLength(50)] // Khớp với DB 
+        public string? PickupStaffCccdNumber { get; set; }
     }
 }
